caac45c670e469d43a2d669b431ed67a0ecd8b86
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-07-31 00:05:11 -0300

    feat(b12): expose curriculum editor dialogs [skip ci]

---
+++ src/components/admin/CurriculumEditorDialogs.tsx
@@ -0,0 +1,3 @@
+export { ModuleEditorDialog } from "@/components/admin/ModuleEditorDialog";
+export { LessonEditorDialog } from "@/components/admin/LessonEditorDialog";
+export { LessonAssetsDialog } from "@/components/admin/LessonAssetsDialog";
