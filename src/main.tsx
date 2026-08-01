e17c73b38ec2c10643478a4f6b59becd4c79a20d
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-01 00:00:18 -0300

    feat(b31): install global frontend error handlers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
+import "./index.css";
+import { installGlobalFrontendErrorHandlers } from "@/observability/frontend-error-reporting";
+
+installGlobalFrontendErrorHandlers();
 
 createRoot(document.getElementById("root")!).render(<App />);
