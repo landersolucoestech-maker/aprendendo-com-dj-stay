d1b7701c82d6778df948d984bfbc8424c3b36a3e
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-01 20:11:49 -0300

    feat(b39): lazy load student support page

--- src/routing/lazy/student-pages.ts
+++ src/routing/lazy/student-pages.ts
@@ -3,6 +3,9 @@ import { lazy } from "react";
 export const StudentPortal = lazy(
   () => import("@/pages/student/StudentPortalRouter"),
 );
+export const StudentSupport = lazy(
+  () => import("@/pages/student/StudentSupport"),
+);
 export const Certificates = lazy(() => import("@/pages/student/Certificates"));
 export const EditProfile = lazy(() => import("@/pages/EditProfile"));
 export const Lesson = lazy(() => import("@/pages/Lesson"));
